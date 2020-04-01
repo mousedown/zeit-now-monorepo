5462da51b125f47ed73af555dee56ed3940ee324
Chris Anthony <chris@theva.ninja>
2020-04-01 08:43:08 +0100

    Separate zeit deployments

--- packages/www-a/index.js
+++ packages/www-a/index.js
@@ -1,5 +1,5 @@
 const greeting = require('@zeit-now-monorepo/shared');
-
+console.log(greeting.hello('World'));
 module.exports = (req, res) => {
   res.end(greeting.hello('World'));
 };
--- packages/www-b/index.js
+++ packages/www-b/index.js
@@ -1,5 +1,5 @@
 const greeting = require('@zeit-now-monorepo/shared');
-
+console.log(greeting.hello('Universe'));
 module.exports = (req, res) => {
   res.end(greeting.hello('Universe'));
 };
